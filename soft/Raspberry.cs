9654100f9e5641f5a770d344842cfb5b5aa686b3
Zane6888 <simon.wohlfart@gmx.at>
2014-03-20 08:34:16 +0100

    Raspberry class

---
+++ soft/Raspberry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace syncsoft
+{
+    class Raspberry
+    {
+
+        public static List<Raspberry> discoverRaspberrys()
+        {
+            //TODO implement method
+            return new List<Raspberry>();
+        }
+
+    }
+}
