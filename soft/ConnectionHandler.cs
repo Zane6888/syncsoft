94807e0e616f03c6566a1beac999501e141d73f6
Zane6888 <simon.wohlfart@gmx.at>
2014-04-03 09:43:48 +0200

    Connection handlers, client info
    Added abstract class for connection handling

---
+++ soft/ConnectionHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace syncsoft
+{
+    /// <summary>
+    /// Used to handle any connection
+    /// </summary>
+    public abstract class ConnectionHandler
+    {
+        public static ConnectionHandler getProtocolHandler(String protocolName)
+        {
+            switch(protocolName)
+            {
+                case "PCCP":
+                    return new PCCPHandler();
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Syncronize files  
+        /// </summary>
+        /// <param name="files">Files and directorys to syncronize, </param>
+        /// <returns>Operation Sucessfull</returns>
+        public abstract bool sync(List<String> files);
+        /// <summary>
+        /// Reqest List of Client from Server
+        /// </summary>
+        /// <returns>List of Clients</returns>
+        public abstract List<ClientInfo> getClientList();
+        /// <summary>
+        /// Request List of Files from Server
+        /// </summary>
+        /// <returns>List of Files</returns>
+        public abstract List<String> getFileList();
+
+    }
+}
