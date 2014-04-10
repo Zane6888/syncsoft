4dc37d0271866e11d96de7c070b1a39a497c26b9
Zane6888 <simon.wohlfart@gmx.at>
2014-04-10 14:55:44 +0200

    Add PacketTypes

---
+++ soft/PacketTypes.cs
@@ -0,0 +1,15 @@
+﻿enum PacketTypes : byte
+{
+    Discover = 1,
+    DiscoverReply = 2,
+    SyncInit = 3,
+    DataRequest = 4,
+    DataSend = 5,
+    SyncFinish = 6,
+    ClientListRequest = 7,
+    ClientList = 8,
+    FileListRequest = 9,
+    FileList = 10,
+    ErrorProtocol = 11,
+    ErrorSystem = 12
+}
