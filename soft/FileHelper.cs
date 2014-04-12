6a67971b3476dacbeaacd3c7e870e1cdb85d61ba
Zane6888 <simon.wohlfart@gmx.at>
2014-04-12 20:41:14 +0200

    Add FileHelper

---
+++ soft/FileHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace syncsoft
+{
+    class FileHelper
+    {
+
+        public static String GetAbsolute(String relative)
+        {
+            //TODO read from config
+            return relative;
+        }
+
+        public static String GetRelative(String absolute)
+        {
+            //TODO read from config
+            return absolute;
+        }
+
+    }
+}
