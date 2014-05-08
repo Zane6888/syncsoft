e5c81ea11f9f91111fb03a0a456265e28197aff8
Zane6888 <simon.wohlfart@gmx.at>
2014-05-08 09:35:06 +0200

    add PCCP Tests

---
+++ RaspberryTests/PCCPHandlerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using syncsoft;
+using System.Net;
+using System.IO;
+using NUnit.Framework;
+
+namespace RaspberryTests
+{
+    [TestFixture]    
+    class PCCPHandlerTest
+    {
+        [Test]
+        public void testSync()
+        {
+            Config.init();
+            File.Create("test_1.txt");
+            Config.addPath("/text_1.txt", Path.GetFullPath("text_1.txt"));
+            PCCPHandler testHandler = new PCCPHandler(IPAddress.Loopback);
+        }
+
+    }
+}
